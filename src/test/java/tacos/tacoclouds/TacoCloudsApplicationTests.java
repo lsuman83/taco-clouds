271810b9bbec860be220c65bcb61bb45c41b0ddf
lsuman83 <sean.j.bordelon@gmail.com>
2021-11-09 15:37:13 -0500

    creating homepage with tests

---
+++ src/test/java/tacos/tacoclouds/TacoCloudsApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.tacoclouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
